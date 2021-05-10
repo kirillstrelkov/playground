84d85d19a400a61e14d6069376ecd0b2432e303f
Kirill Strelkov <kirill.strelkov@gmail.com>
2021-05-10 15:01:54 +0200

    refactor: stock analysis separate class for col vals

--- stocks/buy_sell_analysis/analysis.py
+++ stocks/buy_sell_analysis/analysis.py
@@ -12,6 +12,23 @@ START_DATE = "2011-01-01"
 END_DATE = "2019-01-01"
 
 
+class Column(object):
+    OPEN = "Open"
+    YEAR = "year"
+    MONTH = "month"
+    DAY = "day"
+    HOUR = "hour"
+    MINUTE = "minute"
+    DAY_NAME = "day_name"
+    MONTH_NAME = "month_name"
+    WEEKDAY = "weekday"
+    WEEK = "week"
+    DATE = "Date"
+    SYMBOL = "Symbol"
+    PERCENT = "diff"
+    HISTORY = "history"
+
+
 def __get_history(symbols, interval):
     df = yf.download(
         symbols, interval=interval, start=START_DATE, end=END_DATE, group_by="ticker"
@@ -23,23 +40,29 @@ def __get_history(symbols, interval):
 def __update_dataframe(df):
     df = df.reset_index()
 
-    for component in ["year", "month", "day", "hour", "minute"]:
-        df[component] = df["Date"].apply(lambda x: getattr(x, component))
+    for component in [
+        Column.YEAR,
+        Column.MONTH,
+        Column.DAY,
+        Column.HOUR,
+        Column.MINUTE,
+    ]:
+        df[component] = df[Column.DATE].apply(lambda x: getattr(x, component))
 
-    df["day_name"] = df["Date"].apply(lambda x: x.day_name())
-    df["month_name"] = df["Date"].apply(lambda x: x.month_name())
-    df["weekday"] = df["Date"].apply(lambda x: x.weekday())
-    df["week"] = df["Date"].apply(lambda x: x.isocalendar()[1])
+    df[Column.DAY_NAME] = df[Column.DATE].apply(lambda x: x.day_name())
+    df[Column.MONTH_NAME] = df[Column.DATE].apply(lambda x: x.month_name())
+    df[Column.WEEKDAY] = df[Column.DATE].apply(lambda x: x.weekday())
+    df[Column.WEEK] = df[Column.DATE].apply(lambda x: x.isocalendar()[1])
 
     # Filteting NA in Open - this means usually a dividends
-    df = df[df["Open"].notna()]
+    df = df[df[Column.OPEN].notna()]
 
     return df
 
 
 def __get_symbols(filename, limit):
     df = pd.read_csv(os.path.join(os.path.dirname(__file__), filename))
-    symbols = df["Symbol"]
+    symbols = df[Column.SYMBOL]
 
     if limit:
         symbols = symbols[:limit]
@@ -48,23 +71,23 @@ def __get_symbols(filename, limit):
 
 
 def _get_best_weekday_diffs(df_symbols):
-    symbol = df_symbols["symbol"]
-    df = __update_dataframe(df_symbols["history"])
+    symbol = df_symbols[Column.SYMBOL]
+    df = __update_dataframe(df_symbols[Column.HISTORY])
 
     # if number of working days less than 3 - don't count
     number_of_good_working = 3
 
     # Filter columns
-    df = df[["year", "week", "weekday", "Open"]]
+    df = df[[Column.YEAR, Column.WEEK, Column.WEEKDAY, Column.OPEN]]
 
     df_weeks = DataFrame()
-    for year in df["year"].unique():
-        for week in df["week"].unique():
-            df_week = df[(df["year"] == year) & (df["week"] == week)]
+    for year in df[Column.YEAR].unique():
+        for week in df[Column.WEEK].unique():
+            df_week = df[(df[Column.YEAR] == year) & (df[Column.WEEK] == week)]
             if df_week.empty:
                 continue
 
-            days = df_week["weekday"].values
+            days = df_week[Column.WEEKDAY].values
             if df_week.shape[0] < number_of_good_working:
                 # first and last week of year might contain only 1-2 days
                 if week not in (1, 52, 53):
@@ -73,10 +96,10 @@ def _get_best_weekday_diffs(df_symbols):
                     )
                 continue
 
-            first_weekday = df_week["weekday"].min()
-            df_week["diff"] = (
-                df_week["Open"]
-                / df_week[df_week["weekday"] == first_weekday].iloc[0]["Open"]
+            first_weekday = df_week[Column.WEEKDAY].min()
+            df_week[Column.PERCENT] = (
+                df_week[Column.OPEN]
+                / df_week[df_week[Column.WEEKDAY] == first_weekday].iloc[0][Column.OPEN]
             )
             assert (
                 df_week.shape[0] >= number_of_good_working
@@ -84,7 +107,7 @@ def _get_best_weekday_diffs(df_symbols):
 
             df_weeks = df_weeks.append(df_week)
 
-    return df_weeks[["year", "week", "weekday", "diff"]]
+    return df_weeks[[Column.YEAR, Column.WEEK, Column.WEEKDAY, Column.PERCENT]]
 
 
 def get_best_weekday(filename, limit=None):
@@ -92,25 +115,25 @@ def get_best_weekday(filename, limit=None):
 
 
 def _get_monthly_diffs(df_symbols):
-    symbol = df_symbols["symbol"]
-    df = __update_dataframe(df_symbols["history"])
+    symbol = df_symbols[Column.SYMBOL]
+    df = __update_dataframe(df_symbols[Column.HISTORY])
 
-    df = df.groupby(["year", "month"], as_index=False).mean()
+    df = df.groupby([Column.YEAR, Column.MONTH], as_index=False).mean()
 
     # Filter columns
-    df = df[["year", "month", "Open"]]
+    df = df[[Column.YEAR, Column.MONTH, Column.OPEN]]
 
     df_months = DataFrame()
-    for year in df["year"].unique():
-        df_month = df[df["year"] == year]
+    for year in df[Column.YEAR].unique():
+        df_month = df[df[Column.YEAR] == year]
         if df_month.shape[0] < 12:
             logger.debug(f"Not enough data for {symbol} in {year}")
             continue
 
-        first_month = df_month["month"].min()
-        df_month["diff"] = (
-            df_month["Open"]
-            / df_month[df_month["month"] == first_month].iloc[0]["Open"]
+        first_month = df_month[Column.MONTH].min()
+        df_month[Column.PERCENT] = (
+            df_month[Column.OPEN]
+            / df_month[df_month[Column.MONTH] == first_month].iloc[0][Column.OPEN]
         )
         assert (
             df_month.shape[0] == 12
@@ -118,7 +141,7 @@ def _get_monthly_diffs(df_symbols):
 
         df_months = df_months.append(df_month)
 
-    return df_months[["year", "month", "diff"]]
+    return df_months[[Column.YEAR, Column.MONTH, Column.PERCENT]]
 
 
 def get_best_month(filename, limit=None):
@@ -126,22 +149,22 @@ def get_best_month(filename, limit=None):
 
 
 def _get_month_day_diffs(df_symbols):
-    symbol = df_symbols["symbol"]
-    df = __update_dataframe(df_symbols["history"])
+    symbol = df_symbols[Column.SYMBOL]
+    df = __update_dataframe(df_symbols[Column.HISTORY])
 
     # Filter columns
-    df = df[["year", "month", "day", "Open"]]
+    df = df[[Column.YEAR, Column.MONTH, Column.DAY, Column.OPEN]]
 
     df_months = DataFrame()
-    for year in df["year"].unique():
-        for month in df["month"].unique():
-            df_month = df[(df["year"] == year) & (df["month"] == month)]
+    for year in df[Column.YEAR].unique():
+        for month in df[Column.MONTH].unique():
+            df_month = df[(df[Column.YEAR] == year) & (df[Column.MONTH] == month)]
             if df_month.empty:
                 continue
-            first_day = df_month["day"].min()
-            df_month["diff"] = (
-                df_month["Open"]
-                / df_month[df_month["day"] == first_day].iloc[0]["Open"]
+            first_day = df_month[Column.DAY].min()
+            df_month[Column.PERCENT] = (
+                df_month[Column.OPEN]
+                / df_month[df_month[Column.DAY] == first_day].iloc[0][Column.OPEN]
             )
             if (
                 df_month.shape[0] >= 28 - 10
@@ -150,7 +173,7 @@ def _get_month_day_diffs(df_symbols):
             else:
                 logger.debug(f"Not enough data for {symbol} in {year}.{month}")
 
-    return df_months[["year", "month", "day", "diff"]]
+    return df_months[[Column.YEAR, Column.MONTH, Column.DAY, Column.PERCENT]]
 
 
 def get_best_month_day(filename, limit=None):
@@ -162,7 +185,7 @@ def __wrapper(filename, limit, func, post_func=None, interval="1d"):
 
     history_data = __get_history(symbols.values.tolist(), interval)
     symbols_with_history = [
-        {"symbol": symbol, "history": history_data[symbol]}
+        {Column.SYMBOL: symbol, Column.HISTORY: history_data[symbol]}
         for symbol in history_data.columns.get_level_values(0).unique().to_list()
     ]
 
@@ -170,7 +193,7 @@ def __wrapper(filename, limit, func, post_func=None, interval="1d"):
     if post_func:
         symbols_dfs = post_func(symbols_dfs)
 
-    symbols_dfs["diff"] = symbols_dfs["diff"] * 100
+    symbols_dfs[Column.PERCENT] = symbols_dfs[Column.PERCENT] * 100
 
     symbols_dfs = symbols_dfs.convert_dtypes()
 
--- stocks/buy_sell_analysis/analysis_test.py
+++ stocks/buy_sell_analysis/analysis_test.py
@@ -1,5 +1,6 @@
 import pytest
 from stocks.buy_sell_analysis.analysis import (
+    Column,
     get_best_month,
     get_best_month_day,
     get_best_weekday,
@@ -8,28 +9,31 @@ from stocks.buy_sell_analysis.analysis import (
 
 def test_best_month():
     df = get_best_month("sp500.csv", limit=5)
-    df2018 = df[df["year"] == 2018]
+    df2018 = df[df[Column.YEAR] == 2018]
     assert not df2018.empty
     assert (
-        df2018[df2018["month"] == 1]["diff"].mean()
-        < df2018[df2018["month"] == 11]["diff"].mean()
+        df2018[df2018[Column.MONTH] == 1][Column.PERCENT].mean()
+        < df2018[df2018[Column.MONTH] == 11][Column.PERCENT].mean()
     )
 
 
 def test_best_month_day():
     df = get_best_month_day("sp500.csv", limit=5)
-    df_jan = df[df["month"] == 7]
+    df_jan = df[df[Column.MONTH] == 7]
     assert not df_jan.empty
     assert (
-        df_jan[df_jan["day"] == 1]["diff"].mean()
-        < df_jan[df_jan["day"] == 31]["diff"].mean()
+        df_jan[df_jan[Column.DAY] == 1][Column.PERCENT].mean()
+        < df_jan[df_jan[Column.DAY] == 31][Column.PERCENT].mean()
     )
 
 
 def test_best_weekday():
     df = get_best_weekday("sp500.csv", limit=5)
     assert not df.empty
-    assert df[df["weekday"] == 0]["diff"].mean() < df[df["weekday"] == 4]["diff"].mean()
+    assert (
+        df[df[Column.WEEKDAY] == 0][Column.PERCENT].mean()
+        < df[df[Column.WEEKDAY] == 4][Column.PERCENT].mean()
+    )
 
 
 @pytest.mark.skip(reason="wip")

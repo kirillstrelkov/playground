5701cadb5e292cbbd57e33215bb83bd69605d5bd
Kirill Strelkov <kirill.strelkov@gmail.com>
2023-03-29 19:45:31 +0200

    update mnt

--- auto/mnt/mnt_sum.py
+++ auto/mnt/mnt_sum.py
@@ -1,8 +1,28 @@
 import os
-from collections import defaultdict
-from pprint import pprint
+import re
 
 import pandas as pd
+
+PRIVATE_CUSTOMER = "FÜÜSILINE"
+
+COLUMN_SHORT_NAME = "short name"
+COLUMN_CUSTOMER = "Tüüp (isik)"
+COLUMN_COUNT = "Arv"
+COLUMN_CITY = "Linn"
+COLUMN_REG_DATE = "Esm reg aasta"
+COLUMN_ENGINE_TYPE = "Mootori tüüp"
+COLUMNS = [
+    "Mark",
+    "Mudel",
+    COLUMN_SHORT_NAME,
+    "Mootori tüüp",
+    "Mootori maht",
+    "Mootori võimsus",
+    COLUMN_CITY,
+    COLUMN_CUSTOMER,
+    "Arv",
+    "Värv",
+]
 
 
 def get_summary(folder):
@@ -21,12 +41,31 @@ def get_summary(folder):
         df = pd.read_excel(io=f, sheet_name="Uued sõidukid", skiprows=3)
         df = df[df.Kategooria.apply(lambda x: "M1" in str(x))]
 
+        # replace columns
+        columns_mappings = {"Väljalaske aasta": COLUMN_REG_DATE}
+        if COLUMN_CUSTOMER not in df.columns:
+            columns_mappings.update({"Tüüp": COLUMN_CUSTOMER})
+
+        df = df.rename(columns=columns_mappings)
+
+        year = (
+            df[COLUMN_REG_DATE]
+            .value_counts(dropna=True, ascending=False)
+            .reset_index()["index"]
+            .tolist()[0]
+        )
+        df = df[df[COLUMN_REG_DATE] == year]
+        df[COLUMN_REG_DATE] = pd.to_numeric(df[COLUMN_REG_DATE], downcast="integer")
+
         dframes.append(df)
 
     df = pd.concat(dframes)
     for col in [
         "Mark",
         "Mudel",
+        "Mootori tüüp",
+        COLUMN_CITY,
+        COLUMN_CUSTOMER,
     ]:
         df[col] = df[col].astype(str)
 
@@ -37,22 +76,110 @@ def get_summary(folder):
     mark_namings = {"ŠKODA": "SKODA", "BMW I": "BMW"}
     df["Mark"] = df["Mark"].apply(lambda r: mark_namings.get(r, r))
 
+    df["name"] = df["Mark"] + " " + df["Mudel"]
+
+    # add short name
+    def _get_name_by_split(name, times=2):
+        return " ".join(name.split(" ")[:times])
+
+    def _get_bmw(name):
+        name = name.replace("W M", "W ")
+        if re.search(r"BMW [\d]", name):
+            return name[:5]
+        else:
+            return _get_name_by_split(name)
+
+    def _replace(text, mappings):
+        for _old, _new in mappings.items():
+            text = text.replace(_old, _new)
+        return text
+
+    def _fix_name(name):
+        mark = name.split()[0]
+        name_with_2_words = {
+            "BENTLEY",
+            "CUPRA",
+            "FIAT",
+            "HONDA",
+            "MAZDA",
+            "MERCEDES-BENZ",
+            "MINI",
+            "NISSAN",
+            "PORSCHE",
+            "RENAULT",
+            "SEAT",
+            "SKODA",
+            "VOLKSWAGEN",
+            "VOLVO",
+        }
+        special_names = {
+            "AUDI": lambda n: _get_name_by_split(
+                _replace(n, {" S": " A", " RS ": " A", "AQ": "Q", "AUDI AE": "AUDI E"})
+            ),  # default 2 words
+            "ALFA": lambda n: _get_name_by_split(n, 3),
+            "BMW": _get_bmw,
+            "CITROEN": lambda n: _replace(n, {"E-": ""}),
+            "HYUNDAI": lambda n: _replace(n, {"0N": "0", "0 N": "0"}),
+            "LEXUS": lambda n: n[:8],
+            "OPEL": lambda n: _get_name_by_split(_replace(n, {"ASTRA+": "ASTRA"})),
+            "TOYOTA": lambda n: _replace(
+                n,
+                {
+                    " PHV": "",
+                    " GR ": " ",
+                    " PLUS": "",
+                },
+            ),
+        }
+        if mark in name_with_2_words:
+            return _get_name_by_split(name)
+        elif special_names.get(mark):
+            func = special_names.get(mark)
+            return func(name)
+        else:
+            return name
+
+    df[COLUMN_SHORT_NAME] = df["name"].apply(_fix_name)
+
+    # Fix customers
+    df[COLUMN_CUSTOMER] = (
+        df[COLUMN_CUSTOMER].str.upper().apply(lambda c: c.replace(" ISIK", ""))
+    )
+
+    # Fix city
+    df[COLUMN_CITY] = (
+        df[COLUMN_CITY]
+        .str.replace(" linn", "")
+        .str.strip()
+        .apply(lambda c: {"Narva- Jõesuu": "Narva-Jõesuu"}.get(c, c))
+    )
+
+    # Fix engine types
+    df[COLUMN_ENGINE_TYPE] = (
+        df[COLUMN_ENGINE_TYPE]
+        .str.upper()
+        .apply(
+            lambda t: "CNG"
+            if "CNG" in t
+            else _replace(t, {" ": "_", "Ü": "Y", "KAT.": "KATALYSAATOR"})
+        )
+    )
+
+    assert len(df.columns) == len(set(df.columns))
+
     return df
 
 
 def get_model_stats(df):
-    group_columns = [
-        "Mark",
-        "Mudel",
-    ]
     columns = [
-        "Mark",
-        "Mudel",
-        "Arv",
+        COLUMN_SHORT_NAME,
+        COLUMN_COUNT,
     ]
 
-    return (
-        (df[columns].groupby(group_columns).sum())
+    tmp_df = (
+        (df[columns].groupby(COLUMN_SHORT_NAME).sum([COLUMN_COUNT]))
         .reset_index()
-        .sort_values("Arv", ascending=False)
+        .sort_values(COLUMN_COUNT, ascending=False)
     )
+    tmp_df["Mark"] = tmp_df[COLUMN_SHORT_NAME].str.split(expand=True)[0]
+    return tmp_df[["Mark", COLUMN_SHORT_NAME, COLUMN_COUNT]]
--- auto/mnt/mnt_test.py
+++ auto/mnt/mnt_test.py
@@ -1,36 +1,47 @@
 import os
+import re
 
 import pytest
 
-from auto.mnt.mnt_sum import get_model_stats, get_summary
+from auto.mnt.mnt_sum import (
+    COLUMN_CITY,
+    COLUMN_CUSTOMER,
+    COLUMN_ENGINE_TYPE,
+    PRIVATE_CUSTOMER,
+    get_model_stats,
+    get_summary,
+)
+
+__YEARS = list(range(2018, 2023))
 
 
 @pytest.mark.parametrize(
-    "year,count",
+    "year,count,bestseller",
     [
-        ("2020", 1400),
-        ("2022", 1526),
+        (2018, 925, "RENAULT CLIO"),
+        (2019, 1194, "TOYOTA RAV4"),
+        (2020, 1400, "TOYOTA RAV4"),
+        (2021, 1440, "TOYOTA RAV4"),
+        (2022, 1526, "TOYOTA RAV4"),
     ],
 )
-def test_get_model_stats(year, count):
-    data_dir = os.path.join(os.path.dirname(__file__), "data", year)
+def test_get_model_stats(year, count, bestseller):
+    top_mark, _ = bestseller.split()
+    data_dir = os.path.join(os.path.dirname(__file__), "data", str(year))
     df = get_summary(data_dir)
     stats = get_model_stats(df)
     top = stats.iloc[0]
-    assert top["Mark"] == "TOYOTA"
-    assert top["Mudel"] == "RAV4"
+    assert top["Mark"] == top_mark
+    assert top["short name"] == bestseller
     assert top["Arv"] == count
 
 
 @pytest.mark.parametrize(
     "year",
-    [
-        ("2020"),
-        ("2022"),
-    ],
+    __YEARS,
 )
 def test_fix_names(year):
-    data_dir = os.path.join(os.path.dirname(__file__), "data", year)
+    data_dir = os.path.join(os.path.dirname(__file__), "data", str(year))
     df = get_summary(data_dir)
     marks = df["Mark"].unique().tolist()
 
@@ -39,3 +50,134 @@ def test_fix_names(year):
 
     assert "BMW I" not in marks
     assert "BMW" in marks
+
+
+@pytest.mark.parametrize(
+    "year",
+    __YEARS,
+)
+def test_reg_date(year):
+    data_dir = os.path.join(os.path.dirname(__file__), "data", str(year))
+    df = get_summary(data_dir)
+    assert [int(year)] == df["Esm reg aasta"].unique().tolist()
+
+
+@pytest.mark.parametrize(
+    "year",
+    __YEARS,
+)
+def test_short_names(year):
+    data_dir = os.path.join(os.path.dirname(__file__), "data", str(year))
+    df = get_summary(data_dir)
+    short_names = [
+        "ALFA ROMEO GIULIA",
+        "AUDI A1",
+        "AUDI A6",
+        "BENTLEY BENTAYGA",
+        "BMW 3",
+        "CITROEN C3 AIRCROSS",
+        "CUPRA BORN",
+        "CUPRA FORMENTOR",
+        "HYUNDAI I 20",
+        "HYUNDAI I 30",
+        "HYUNDAI SANTA FE",
+        "LEXUS ES",
+        "LEXUS NX",
+        "MERCEDES-BENZ AMG",
+        "NISSAN LEAF",
+        "OPEL ASTRA",
+        "PORSCHE 911",
+        "RENAULT ARKANA",
+        "SEAT LEON",
+        "SKODA ENYAQ",
+        "SKODA OCTAVIA",
+        "TOYOTA PRIUS",
+        "TOYOTA YARIS CROSS",
+        "VOLKSWAGEN ID.4",
+    ]
+    unique_names = set(df["short name"].unique().tolist())
+    for grouped_name in short_names:
+        filtered_names = [name for name in unique_names if grouped_name in name]
+        if filtered_names:
+            assert grouped_name in filtered_names
+            assert len(filtered_names) == 1
+
+    for name in [
+        "AUDI RS 3",
+        "AUDI S3 LIMOUSINE",
+        "AUDI SQ2",
+        "BMW M5",
+        "HYUNDAI I 30N",
+        "TOYOTA GR YARIS",
+    ]:
+        assert name not in unique_names
+
+
+@pytest.mark.parametrize(
+    "year",
+    __YEARS,
+)
+def test_private_customers(year):
+    data_dir = os.path.join(os.path.dirname(__file__), "data", str(year))
+    df = get_summary(data_dir)
+
+    values = df[COLUMN_CUSTOMER].unique().tolist()
+    values_private = [v for v in values if re.search("F.+NE", v)]
+    assert [PRIVATE_CUSTOMER] == values_private
+
+
+@pytest.mark.parametrize(
+    "year",
+    __YEARS,
+)
+def test_cities(year):
+    data_dir = os.path.join(os.path.dirname(__file__), "data", str(year))
+    df = get_summary(data_dir)
+
+    values = set(df[COLUMN_CITY].unique().tolist())
+
+    assert not values.difference(
+        {
+            "Haapsalu",
+            "Keila",
+            "Kohtla-Järve",
+            "Loksa",
+            "Maardu",
+            "Määramata",
+            "Narva",
+            "Narva-Jõesuu",
+            "Paide",
+            "Pärnu",
+            "Rakvere",
+            "Sillamäe",
+            "Tallinn",
+            "Tartu",
+            "Viljandi",
+            "Võru",
+            "Tähtvere vald",
+            "Põltsamaa",
+            "Rapla",
+        }
+    )
+
+
+@pytest.mark.parametrize(
+    "year",
+    __YEARS,
+)
+def test_engine_types(year):
+    data_dir = os.path.join(os.path.dirname(__file__), "data", str(year))
+    df = get_summary(data_dir)
+
+    values = set(df[COLUMN_ENGINE_TYPE].unique().tolist())
+    assert not values.difference(
+        {
+            "ELEKTER",
+            "DIISEL",
+            "DIISEL_HYBRIID",
+            "BENSIIN",
+            "BENSIIN_KATALYSAATOR",
+            "BENSIIN_HYBRIID",
+            "CNG",
+        }
+    )
